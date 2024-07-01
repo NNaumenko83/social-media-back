40d70cc3611379f78c2470578d5b8b405b7ebc91
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-07-01 23:25:16 +0300

    UPD: Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const ctrlWrapper = require('../helpers/ctrlWrapper');
+const bcrypt = require('bcrypt');
 
 // REGISTER
-router.post('/register', async (req, res) => {
-    const user = await User.create(req.body);
+router.post(
+    '/register',
+    ctrlWrapper(async (req, res) => {
+        // Hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        console.log('hashedPassword:', hashedPassword);
 
-    res.status(201).json('register success');
+        // Create new user
+        const user = new User({ ...req.body, password: hashedPassword });
+
+        // Save user to database
+        await user.save();
+
+        res.status(201).json('User registered successfully');
+    }),
+);
+
+// LOGIN
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return res.status(401).json('Invalid email or password');
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+        return res.status(401).json('Invalid email or password');
+    }
+
+    res.json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    });
 });
 
 module.exports = router;
